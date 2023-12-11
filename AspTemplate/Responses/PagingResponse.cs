c560276462ff3073ffa789c177ebeff06b59d131
kn <nguyentuanpkn@gmail.com>
2023-12-11 14:37:40 +0700

    PagingResponse

--- AspTemplate/Responses/PagingResponse.cs
+++ AspTemplate/Responses/PagingResponse.cs
@@ -2,5 +2,5 @@ public class PagingResponse<T> : RangeResponse<T>
 {
     public required int Size;
     public required int Total { get; set; }
-    public int MaxPage { get { return (int)Math.Ceiling(((double)Total / (double)Size)); } }
+    public int MaxPage { get { return (int)Math.Ceiling((double)Total / Size); } }
 }

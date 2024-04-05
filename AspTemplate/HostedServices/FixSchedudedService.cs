164e6b4771cdfa754724154f8cdbdf0f56394bff
ndknitor <nguyentuanpkn@gmail.com>
2024-04-05 09:43:51 +0700

    Format FixSchedudedService

--- AspTemplate/HostedServices/FixSchedudedService.cs
+++ AspTemplate/HostedServices/FixSchedudedService.cs
@@ -15,18 +15,13 @@ public class FixSchedudedService : BackgroundService
         {
             DateTime now = DateTime.Now;
             DateTime nextRunTime = new DateTime(now.Year, now.Month, now.Day, 11, 0, 0); // Next occurrence at 11:00
-            
             if (now > nextRunTime)
             {
                 nextRunTime = nextRunTime.AddDays(1); // If it's past 11:00, set it to the next day
             }
-
             TimeSpan delay = nextRunTime - now;
-
             _logger.LogInformation("Next scheduled task will run at: {time}", nextRunTime);
-
             await Task.Delay(delay, stoppingToken); // Delay until next occurrence
-
             _logger.LogInformation("Scheduled task is running at: {time}", DateTimeOffset.Now);
 
             // Perform your scheduled task logic here
